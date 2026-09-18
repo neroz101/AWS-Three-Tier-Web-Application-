2e3d87c9c231cdc10655f7ed78eb0d9d77d79676
neroz101 <neroz101@outlook.com>
2026-09-18 20:39:32 +0200

    Add database configuration in DbConfig.js

---
+++ app/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'db-tier.cnas4osg07ed.eu-north-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'tonymuokem2026',
+    DB_DATABASE : 'tonydb'
+});
